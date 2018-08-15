29e9e4cc5fe5963c28394ac0df176cd740691933
pon32258 <pon1852@hotmail.com>
2018-08-15 21:46:47 +0700

    Remove radius

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,18 +1,12 @@
 import React from 'react';
-import Radium from 'radium';
 
 import './Person.css'
 
 const person = (props) => { 
 
-    const style ={
-        '@media (min-width: 500px)':{
-            width: '450px'
-        }
-    };
-    
+   
     return ( 
-    <div className="Person" style={style}>
+    <div className="Person">
         <p onClick={props.click}> I 'm a Person and I am {props.name} years {props.age} old!</p>
         <p>{props.children}</p>
         <input type="text" onChange={props.changed} value={props.name} />
@@ -24,4 +18,4 @@ const person = (props) => {
 
 
 
-export default Radium(person);
+export default person;
